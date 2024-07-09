ab4293d082dd2f863949d85919a4fc4d4a24c46d
UNIemd <129679888+UNIemd@users.noreply.github.com>
2024-07-09 19:52:26 +0200

    push codice da finire sui filtri

---
+++ src/main/java/org/unisa/abeilleamorellifontana_pj/Controller/SessionFilter.java
@@ -0,0 +1,31 @@
+package org.unisa.abeilleamorellifontana_pj.Controller;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.unisa.abeilleamorellifontana_pj.Model.SessionManager;
+
+import java.io.IOException;
+
+
+public class SessionFilter extends HttpFilter {
+
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        if (req.getSession(false) == null) {
+            req.getSession().setAttribute("SessionManager", new SessionManager());
+            SessionManager s = new SessionManager();
+            if (s.isLoggato()) {
+                System.out.println("non da falso");
+            }
+            if (s.isAdmin()) {
+                System.out.println("ancora più falso");
+            }
+        }
+
+        chain.doFilter(req, res);
+    }
+}
